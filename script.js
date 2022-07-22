51b5b2613a301838f30f0853219d4d422c1dc80f
PATRICK CULLETON <pcoreyculleton@gmail.com>
2022-07-21 20:02:49 -0500

    done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+window.addEventListener("load",function() {
+   fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+    response.json().then(function(json) {
+        const div = document.getElementById('container');
+        for(i = 0; i < json.length; i++) {
+            div.innerHTML += `
+            <div class ='astronaut'>
+                <div class = 'bio'>
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hourInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                <img class = 'avatar' src = ${json[i].picture}>
+             </div>`
+        }
+    })
+   })
+})
+
 // TODO: add code here
